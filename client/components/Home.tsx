b1e2dd977423aa452437d7de566cc4a50bf1c520
Matt Spriggs <83927528+mattspriggs@users.noreply.github.com>
2023-11-03 10:22:53 +1300

    Generating random array for the PC to play

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -2,11 +2,20 @@ import Color from './Color'
 
 const color: string[] = ['red', 'green', 'yellow', 'blue']
 const player: string[] = []
+const patternLength = 10
 const pc: string[] = []
-function getRandomColor(array: string[]) {
-  return array.push(color[Math.floor(Math.random() * color.length - 1)])
+function getRandomIndex(num: number) {
+  return Math.floor(Math.random() * num)
 }
-console.log(getRandomColor(pc))
+
+function generatePc() {
+  for (let i = 0; i < patternLength; i++) {
+    pc.push(color[getRandomIndex(color.length - 1)])
+  }
+}
+generatePc()
+console.log(getRandomIndex(color.length))
+console.log(pc)
 
 function Home() {
   return (
